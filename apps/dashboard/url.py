5fb9eab41b02afccd8baf92b75b55e518211567b
Charles Mahouve <72011944+charlesMahouve@users.noreply.github.com>
2022-06-29 15:59:53 +0200

    pushing the mod files and new files

--- apps/dashboard/url.py
+++ apps/dashboard/url.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import dashboard
-
+from apps.testzone.views import testzone, individualtestzone, testzone_add, testzone_edit, testzone_delete
 urlpatterns = [
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('testzone/', testzone, name='testzone'),
+    path('testzone/add/', testzone_add, name='testzone_add'),
+    path('testzone/<int:individualtestzone_id>/edit/', testzone_edit, name='testzone_edit'),
+    path('testzone/<int:individualtestzone_id>/delete/', testzone_delete, name='testzone_delete'),
+    path('testzone/<int:individualtestzone_id>/', individualtestzone, name='individualtestzone'),
 ]
--- secad/settings.py
+++ secad/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     # 'apps.core.apps.AppConfig',
     'apps.core',
     'apps.dashboard',
+    'apps.testzone',
+
 
 ]
 
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
